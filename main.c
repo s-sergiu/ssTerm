25295e8df6c3c05649354452e18fcdb086565316
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-09-09 21:11:24 +0200

    implemented fork

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -23,6 +25,37 @@ int open_pty_pair (int *amaster, int *aslave) {
   return 1;
 }
 
+char buffer[1000];
+int create_p(int *master, int *slave) {
+	int p;
+	char *env = NULL;
+
+	p = fork();
+	if (p == 0)
+	{
+		close(*master);
+
+		setsid();
+		if (ioctl(*slave, TIOCSCTTY, NULL) == -1)
+		{
+			perror("ioctl(TIOCSCTTY)");
+			return 0;
+		}
+
+		dup2(*slave, 0);
+		dup2(*slave, 1);
+		dup2(*slave, 2);
+		close(*slave);
+
+		printf("executing \n");
+		execle("/bin/bash", "/bin/who", NULL, env);
+	}
+	while(waitpid(p, NULL, WNOHANG) > 0)
+		;
+	read(*master, buffer, 1000);
+	return 1;
+}
+
 int main(void) {
 	int master;
 	int slave;
@@ -31,5 +64,9 @@ int main(void) {
 	result = open_pty_pair(&master, &slave);
 	printf("result is: %d\n", result);	
 	printf("master is %d and slave is %d \n", master, slave);	
+
+	printf("Creating process\n");	
+	create_p(&master,&slave);
+	printf("stopping process\n");	
 	return (0);
 }
